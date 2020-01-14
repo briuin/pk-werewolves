2cc423045993a9637ae569955f15c43a5d417823
Edward Lim <Edward.Lim@redcypress.com.tw>
2020-01-14 14:11:51 +0800

    add vue socket io

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueSocketIO from "vue-socket.io";
 
 Vue.config.productionTip = false;
+
+Vue.use(new VueSocketIO({
+  connection: `http://localhost:3003/chat`,
+    options: { useConnectionNamespace: true },
+    debug: false,
+}));
+
+Vue.use(new VueSocketIO({
+  connection: `http://localhost:3003/werewolves`,
+    options: { useConnectionNamespace: true },
+    debug: false,
+}));
 
 new Vue({
   router,
---
+++ src/typings/vue-socket.io.d.ts
@@ -0,0 +1,4 @@
+declare module "vue-socket.io" {
+  const value: any;
+  export default value;
+}
