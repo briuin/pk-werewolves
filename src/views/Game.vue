9403ce7f650bb2ed5c8292c77e0d232e293f664e
Edward Lim <Edward.Lim@redcypress.com.tw>
2020-02-09 13:34:04 +0800

    list player

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -14,9 +14,38 @@
               </template>
             </v-expansion-panel-header>
             <v-expansion-panel-content>
-              <p>已準備人數：{{ isReadyPlayers.length }}</p>
+              <!--<p>已準備人數：{{ isReadyPlayers.length }}</p>
               <p>坐下人數：{{ seatedPlayers.length }}</p>
-              <p>旁觀人數：{{ players.length - seatedPlayers.length }}</p>
+              <p>旁觀人數：{{ players.length - seatedPlayers.length }}</p>-->
+              <v-list>
+                <v-list-group :value="true" no-action prepend-icon="account_circle">
+                  <template v-slot:activator>
+                    <v-list-item-title>就坐玩家</v-list-item-title>
+                  </template>
+
+                  <v-list-item v-for="(player, i) in seatedPlayers" :key="'ready' + i">
+                    <v-list-item-title
+                      v-text="player.name + (isReady(player.name) ? ' - 準備就緒' : '')"
+                    ></v-list-item-title>
+                    <v-list-item-action>
+                      <v-icon v-if="player.isBot" color="red" @click="removeBot(player.name)">delete</v-icon>
+                      <v-icon v-else>account_circle</v-icon>
+                    </v-list-item-action>
+                  </v-list-item>
+                </v-list-group>
+                <v-list-group :value="false" no-action prepend-icon="account_circle">
+                  <template v-slot:activator>
+                    <v-list-item-title>旁觀玩家</v-list-item-title>
+                  </template>
+
+                  <v-list-item v-for="(player, i) in observers" :key="'ready' + i">
+                    <v-list-item-title v-text="player.name"></v-list-item-title>
+                    <v-list-item-action>
+                      <v-icon>account_circle</v-icon>
+                    </v-list-item-action>
+                  </v-list-item>
+                </v-list-group>
+              </v-list>
             </v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
@@ -34,7 +63,8 @@
             </v-expansion-panel-header>
             <v-expansion-panel-content>
               <p>{{ cards.join(", ") }}</p>
-              <v-btn @click="$socket.werewolves.emit('addRobot')">新增電腦</v-btn>
+              <v-btn @click="$socket.werewolves.emit('addBot')">新增電腦</v-btn>
+              <p v-for="(seat, i) in seatedPlayers" :key="`seat${i}`">{{ i + 1}} : {{ seat.name }}</p>
             </v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
@@ -109,7 +139,7 @@ export default class Game extends Vue {
   messages: any[] = [];
   owner = "";
   isStarted = false;
-  players = [];
+  players: any[] = [];
   seatedPlayers: any[] = [];
   isReadyPlayers: any[] = [];
   card: Card = new Card();
@@ -117,8 +147,22 @@ export default class Game extends Vue {
   seatNo = 0;
   seated = false;
 
+  get observers() {
+    return this.players.filter(
+      x => !this.seatedPlayers.find(s => s.name === x.name)
+    );
+  }
+
   get isOwner() {
     return this.owner === PlayerService.getName();
+  }
+
+  isReady(name: string) {
+    return !!this.isReadyPlayers.find(x => x.name === name);
+  }
+
+  removeBot(name: string) {
+    this.$socket.werewolves.emit("removeBot", { botName: name });
   }
 
   reset() {
